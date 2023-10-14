22e4f1424b404a32efa894c48d4b8521457b890c
erkinbek1 <erkinbekzhusubaliev@gmail.com>
2023-10-14 17:13:40 +0200

    Initial commit

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.example.helloworld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Vistula1");
+        System.out.println("Hello Vistula2");
+        System.out.println("Hello Vistula3");
+        System.out.println("Hello Vistula4");
+        System.out.println("Hello Vistula5");
+
+    }
+}
